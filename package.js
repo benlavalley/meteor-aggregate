93bd1cc5c05eea6db7193c2fdce32591069238e6
Ben Lavalley <ben.lavalley@kaseya.com>
2019-10-29 03:26:06 -0400

    version bump

--- package.js
+++ package.js
@@ -1,26 +1,26 @@
 Package.describe({
-  summary: 'Proper MongoDB aggregations support for Meteor',
-  version: '1.4.3',
-  git: 'https://github.com/sakulstra/meteor-aggregate',
-  name: 'sakulstra:aggregate'
+	summary: 'Proper MongoDB aggregations support for Meteor',
+	version: '1.4.4',
+	git: 'https://github.com/sakulstra/meteor-aggregate',
+	name: 'sakulstra:aggregate',
 });
 
-Package.onUse(function(api) {
-  configurePackage(api);
+Package.onUse(function (api) {
+	configurePackage(api);
 });
 
-Package.onTest(function(api) {
-  configurePackage(api);
-  api.use(['tinytest', 'accounts-password', 'random'], ['server']);
+Package.onTest(function (api) {
+	configurePackage(api);
+	api.use(['tinytest', 'accounts-password', 'random'], ['server']);
 
-  // common before
-  api.addFiles(['test.js'], ['server']);
+	// common before
+	api.addFiles(['test.js'], ['server']);
 });
 
 function configurePackage(api) {
-  api.versionsFrom('METEOR@1');
-  api.use(['mongo'], ['server']);
+	api.versionsFrom('METEOR@1.8.1');
+	api.use(['mongo'], ['server']);
 
-  // common before
-  api.addFiles(['index.js'], ['server']);
+	// common before
+	api.addFiles(['index.js'], ['server']);
 }
